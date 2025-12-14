ce31817437cac392d6f5fa765300d086a91af37f
rohithandique <rohit.handique.rh@gmail.com>
2025-12-15 01:22:17 +0530

    added assertion for main method test

--- src/test/java/com/generic/server/ServerApplicationTest.java
+++ src/test/java/com/generic/server/ServerApplicationTest.java
@@ -1,12 +1,13 @@
 package com.generic.server;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ServerApplicationTest {
 
     @Test
     void test_main() {
-        ServerApplication.main(new String[]{"--spring.main.web-application-type=none"});
+        Assertions.assertDoesNotThrow(() -> ServerApplication.main(new String[]{"--spring.main.web-application-type=none"}));
     }
 
 }
